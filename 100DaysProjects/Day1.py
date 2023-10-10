7b0b87cd52317cd8f02dc43a407c89afded6fa3e
LeonardoGagliardo <leogagliardo@hotmail.com>
2023-10-10 17:11:17 -0300

    update

--- 100DaysProjects/Day1.py
+++ 100DaysProjects/Day1.py
@@ -15,9 +15,7 @@ print("Your band name could be " + first_name + " " + second_name)
 
 #print("Welcome to the Band Name Generator.")
 #first_name = input("What's your favorite thing in the whole world (in one word?)\n")
-
 #second_name = input("What makes your heart race? (in one word)\n")
-
 #print("Your band name could be " + first_name + " " + second_name)
 
 
