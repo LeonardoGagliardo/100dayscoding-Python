4d3898fdb98f927f2711da342427568da2e2aabf
LeonardoGagliardo <leogagliardo@hotmail.com>
2023-10-10 17:07:34 -0300

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,23 @@
+#Day 1 Project: Band Name Generator
+
+
+print("Welcome to the Band Name Generator.")
+print("What's your favorite thing in the whole world (in one word?)")
+first_name = input()
+print("What makes your heart race? (in one word)")
+second_name = input()
+print("Your band name could be " + first_name + " " + second_name)
+
+
+
+
+#Another solution:
+
+#print("Welcome to the Band Name Generator.")
+#first_name = input("What's your favorite thing in the whole world (in one word?)\n")
+
+#second_name = input("What makes your heart race? (in one word)\n")
+
+#print("Your band name could be " + first_name + " " + second_name)
+
+
