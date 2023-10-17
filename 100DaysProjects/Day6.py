600d48f3a2bc1387d1268c3bdd2723602f47398f
LeonardoGagliardo <leogagliardo@hotmail.com>
2023-10-16 22:43:51 -0300

    Update Day6.py

--- 100DaysProjects/Day6.py
+++ 100DaysProjects/Day6.py
@@ -1,1 +1,28 @@
-#Day 6 Project: .
+#Day 6 Project: Escaping the Maze
+
+#This project was actually from reeborg.ca:
+
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+#This is the solution of the problem
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+"""
