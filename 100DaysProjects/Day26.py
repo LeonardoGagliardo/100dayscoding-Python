42decde22a941f77104223258cff00184e99d4e4
LeonardoGagliardo <leogagliardo@hotmail.com>
2023-12-02 22:20:52 -0300

    Day 30 Project

--- 100DaysProjects/Day26.py
+++ 100DaysProjects/Day26.py
@@ -2,13 +2,23 @@
 
 import pandas
 
+repeat = True
+
+
 phonetic_alphabet_csv =  pandas.read_csv("Extra_Data/Data26/nato_phonetic_alphabet.csv")
 
 phonetic_alphabet_dict = {row.letter: row.code for (index, row) in phonetic_alphabet_csv.iterrows()}
 
-word = input("Enter a word: ").upper()
-word = [letter for letter in word]
+while repeat:
+    word = input("Enter a word: ").upper()
+    word = [letter for letter in word]
 
-phonetic_alphabet = [phonetic_alphabet_dict[letter] for letter in word]
+    try:
+        phonetic_alphabet = [phonetic_alphabet_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        repeat = False
+        print(phonetic_alphabet)
 
-print(phonetic_alphabet)
+
---
+++ 100DaysProjects/Day30.py
@@ -0,0 +1,3 @@
+#Day 30 Project: Learning Theory 
+
+# This day was dedicated exclusively to learning about JSON and Cathing Exceptions.
